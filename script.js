91df9b130ab11427ee55d4c75e1938e35060ccda
HamsterIR <blitzb1j9@gmail.com>
2024-09-15 22:38:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,246 +1,223 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const EVENTS_DELAY = 20000;
+money = 0;
+moneyup = 1;
+msec = 0;
+upcost = 15;
+catcost = 25;
+workercost = 250;
+upown = 0;
+catown = 0;
+workerown = 0;
+catadd = 1;
+workadd = 15;
+cboost = 1;
+wboost = 1;
+catmax = 0;
+workmax = 0;
 
-    const games = {
-        1: {
-            name: 'Riding Extreme 3D',
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-        },
-        2: {
-            name: 'Chain Cube 2048',
-            appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
-            promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-        },
-        3: {
-            name: 'My Clone Army',
-            appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
-            promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
-        },
-        4: {
-            name: 'Train Miner',
-            appToken: '82647f43-3f87-402d-88dd-09a90025313f',
-            promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-        }
-    };
+//save before exiting
+function closingCode() {
+  if (confirm("You have closed the window, would you like to save?") === true) {
+    save();
+    return null;
+  }
+}
 
-    const startBtn = document.getElementById('startBtn');
-    const keyCountSelect = document.getElementById('keyCountSelect');
-    const keyCountLabel = document.getElementById('keyCountLabel');
-    const progressContainer = document.getElementById('progressContainer');
-    const progressBar = document.getElementById('progressBar');
-    const progressText = document.getElementById('progressText');
-    const progressLog = document.getElementById('progressLog');
-    const keyContainer = document.getElementById('keyContainer');
-    const keysList = document.getElementById('keysList');
-    const copyAllBtn = document.getElementById('copyAllBtn');
-    const generatedKeysTitle = document.getElementById('generatedKeysTitle');
-    const gameSelect = document.getElementById('gameSelect');
-    const copyStatus = document.getElementById('copyStatus');
-    const telegramChannelBtn = document.getElementById('telegramChannelBtn');
+function addcomma(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
+//updates all values
+function reloadall() {
+  document.getElementById("click").innerHTML =
+    "LB/click: " + addcomma(moneyup) + " | LB/sec: " + addcomma(msec);
+  document.getElementById("total").innerHTML = "LB: " + addcomma(money);
+  document.getElementById("cat").innerHTML =
+    catown + "-clicker cat: " + addcomma(catcost) + " | +" + addcomma(catadd) + "/sec";
+  document.getElementById("worker").innerHTML =
+    workerown + "-worker: " + addcomma(workercost) + " | +" + addcomma(workadd) + "/sec";
+  document.getElementById("upgrade").innerHTML =
+    addcomma(upown) + "-main upgrade: " + addcomma(upcost);
+}
+//overwrites save file
+function save() {
+  localStorage.setItem("money", money);
+  localStorage.setItem("moneyup", moneyup);
+  localStorage.setItem("msec", msec);
+  localStorage.setItem("upcost", upcost);
+  localStorage.setItem("catcost", catcost);
+  localStorage.setItem("catadd", catadd);
+  localStorage.setItem("workercost", workercost);
+  localStorage.setItem("workadd", workadd);
+  localStorage.setItem("catown", catown);
+  localStorage.setItem("workerown", workerown);
+  localStorage.setItem("upown", upown);
+  localStorage.setItem("catadd", catadd);
+  localStorage.setItem("workadd", workadd);
+  localStorage.setItem("cboost", cboost);
+  localStorage.setItem("wboost", wboost);
+  localStorage.setItem("catmax", catmax);
+  localStorage.setItem("workmax", workmax);
+}
+//loads save file
+function load() {
+  money = parseInt(localStorage.getItem("money"));
+  moneyup = parseInt(localStorage.getItem("moneyup"));
+  msec = parseInt(localStorage.getItem("msec"));
+  upcost = parseInt(localStorage.getItem("upcost"));
+  catcost = parseInt(localStorage.getItem("catcost"));
+  upown = parseInt(localStorage.getItem("catadd"));
+  workercost = parseInt(localStorage.getItem("workercost"));
+  upown = parseInt(localStorage.getItem("workadd"));
+  catown = parseInt(localStorage.getItem("catown"));
+  workerown = parseInt(localStorage.getItem("workerown"));
+  upown = parseInt(localStorage.getItem("upown"));
+  catadd = parseInt(localStorage.getItem("catadd"));
+  workadd = parseInt(localStorage.getItem("workadd"));
+  cboost = parseInt(localStorage.getItem("cboost"));
+  wboost = parseInt(localStorage.getItem("wboost"));
+  catmax = parseInt(localStorage.getItem("catmax"));
+  workmax = parseInt(localStorage.getItem("workmax"));
 
-    startBtn.addEventListener('click', async () => {
-        const gameChoice = parseInt(gameSelect.value);
-        const keyCount = parseInt(keyCountSelect.value);
-        const game = games[gameChoice];
+  reloadall();
+}
+//resets all values
+function reset() {
+  if (confirm("Are you sure you want to reset?") === true) {
+    money = 0;
+    moneyup = 1;
+    msec = 0;
+    upcost = 15;
+    catcost = 25;
+    workercost = 250;
+    catown = 0;
+    workerown = 0;
+    upown = 0;
+    catadd = 1;
+    workadd = 15;
+    reloadall();
+  }
+}
+//timer
+function myTimer() {
+    money += msec;
+  document.getElementById("total").innerHTML = "LB: " + addcomma(money);
+}
+setInterval(myTimer, 1000);
 
-        keyCountLabel.innerText = `تعداد کلید ها ${keyCount}`;
+//what happens when button is clicked
+function clicked() {
+  money += moneyup;
+  document.getElementById("total").innerHTML = "LB: " + addcomma(money);
+}
+//upgrade function
+function upgrade(name) {
+  if (name == "clicker cat") {
+    if (money >= catcost && catown < 50) {
+      
+      if (catown <= 13) {
+        msec += catadd;
+        catadd++;
+        cboost = 1;
+      } else if (catown == 14) {
+        msec += catadd;
+        catadd++;
+        cboost = 200;
+      } else if (catown <= 23) {
+        msec += 200 * catadd;
+        catadd++;
+        cboost = 200;
+      } else if (catown == 24) {
+        msec += 200 * catadd;
+        catadd++;
+        cboost = 5000;
+      } else if (catown <= 48) {
+        msec += 5000 * catadd;
+        catadd++;
+        cboost = 5000;
+      } else if (catown == 49) {
+        msec += 5000 * catadd;
+        catadd++;
+        cboost = 15000;
+      } else {
+        msec += 15000 * catadd;
+        catadd++;
+        cboost = 15000;
+      }
+      catown += 1;
+      money -= catcost;
+      catcost = catcost * 2;
+      document.getElementById("cat").innerHTML =
+        catown + "-clicker cat: " + addcomma(catcost) + " | +" + addcomma(catadd * cboost) + "/sec";
+    } else if (catown == 50) {
+      document.getElementById("cat").innerHTML =
+        catown + "-clicker cat: MAX | +15% click/sec";
+    }
+  }
 
-        progressBar.style.width = '0%';
-        progressText.innerText = '0%';
-        progressLog.innerText = 'Starting...';
-        progressContainer.classList.remove('hidden');
-        keyContainer.classList.add('hidden');
-        generatedKeysTitle.classList.add('hidden');
-        keysList.innerHTML = '';
-        keyCountSelect.classList.add('hidden');
-        gameSelect.classList.add('hidden');
-        startBtn.classList.add('hidden');
-        copyAllBtn.classList.add('hidden');
-        startBtn.disabled = true;
+  if (name == "worker") {
+    if (money >= workercost && workerown < 50) {
+      
+      if (workerown <= 13) {
+        msec += workadd;
+        workadd++;
+        wboost = 1;
+      } else if (workerown == 14) {
+        msec += workadd;
+        workadd++;
+        wboost = 200;
+      } else if (workerown <= 23) {
+        msec += 200 * workadd;
+        workadd++;
+        wboost = 200;
+      } else if (workerown == 24) {
+        msec += 200 * workadd;
+        workadd++;
+        wboost = 5000;
+      } else if (workerown <= 48) {
+        msec += 5000 * workadd;
+        workadd++;
+        wboost = 5000;
+      } else if (workerown == 49) {
+        msec += 5000 * workadd;
+        workadd++;
+        wboost = 15000;
+      } else {
+        msec += 15000 * workadd;
+        workadd++;
+        wboost = 15000;
+      }
+      workerown += 1;
+      money -= workercost;
+      workercost = workercost * 3;
+      document.getElementById("worker").innerHTML = 
+        workerown + "-worker: " + addcomma(workercost) + " | +" + addcomma(workadd * wboost) + "/sec";
+    } else if (workerown == 50) {
+      document.getElementById("worker").innerHTML =
+        workerown + "-worker: MAX | +35% click/sec";
+    }
+  }
 
-        let progress = 0;
-        const updateProgress = (increment, message) => {
-            progress += increment;
-            progressBar.style.width = `${progress}%`;
-            progressText.innerText = `${progress}%`;
-            progressLog.innerText = message;
-        };
+  if (name == "upgrade") {
+    if (money >= upcost) {
+      moneyup += upcost / 15;
+      money -= upcost;
+      upown += 1;
+      upcost = upcost * 5;
+      document.getElementById("upgrade").innerHTML =
+        addcomma(upown) + "-main upgrade: " + addcomma(upcost);
+      if (catown == 50) {
+        msec -= catmax;
+        catmax = Math.floor(moneyup * 0.15);
+        msec += catmax;
+      }
+      if (workerown == 50) {
+        msec -= workmax;
+        workmax = Math.floor(moneyup * 0.35);
+        msec += workmax;
+      }
+    }
+  }
 
-        const generateKeyProcess = async () => {
-            const clientId = generateClientId();
-            let clientToken;
-            try {
-                clientToken = await login(clientId, game.appToken);
-            } catch (error) {
-                alert(`Failed to login: ${error.message}`);
-                startBtn.disabled = false;
-                return null;
-            }
-
-            for (let i = 0; i < 11; i++) {
-                await sleep(EVENTS_DELAY * delayRandom());
-                const hasCode = await emulateProgress(clientToken, game.promoId);
-                updateProgress(7 / keyCount, '... درحال استخراج');
-                if (hasCode) {
-                    break;
-                }
-            }
-
-            try {
-                const key = await generateKey(clientToken, game.promoId);
-                updateProgress(30 / keyCount, 'Generating key...');
-                return key;
-            } catch (error) {
-                alert(`Failed to generate key: ${error.message}`);
-                return null;
-            }
-        };
-
-        const keys = await Promise.all(Array.from({ length: keyCount }, generateKeyProcess));
-
-        if (keys.length > 1) {
-            keysList.innerHTML = keys.filter(key => key).map(key =>
-                `<div class="key-item">
-                    <input type="text" value="${key}" readonly>
-                    <button class="copyKeyBtn" data-key="${key}">کپی کلید</button>
-                </div>`
-            ).join('');
-            copyAllBtn.classList.remove('hidden');
-        } else if (keys.length === 1) {
-            keysList.innerHTML =
-                `<div class="key-item">
-                    <input type="text" value="${keys[0]}" readonly>
-                    <button class="copyKeyBtn" data-key="${keys[0]}">کپی کلید</button>
-                </div>`;
-        }
-
-        keyContainer.classList.remove('hidden');
-        generatedKeysTitle.classList.remove('hidden');
-        document.querySelectorAll('.copyKeyBtn').forEach(button => {
-            button.addEventListener('click', (event) => {
-                const key = event.target.getAttribute('data-key');
-                navigator.clipboard.writeText(key).then(() => {
-                    copyStatus.classList.remove('hidden');
-                    setTimeout(() => copyStatus.classList.add('hidden'), 2000);
-                });
-            });
-        });
-        copyAllBtn.addEventListener('click', () => {
-            const keysText = keys.filter(key => key).join('\n');
-            navigator.clipboard.writeText(keysText).then(() => {
-                copyStatus.classList.remove('hidden');
-                setTimeout(() => copyStatus.classList.add('hidden'), 2000);
-            });
-        });
-
-        progressBar.style.width = '100%';
-        progressText.innerText = '100%';
-        progressLog.innerText = 'کامل';
-
-        startBtn.classList.remove('hidden');
-        keyCountSelect.classList.remove('hidden');
-        gameSelect.classList.remove('hidden');
-        startBtn.disabled = false;
-    });
-
-    document.getElementById('generateMoreBtn').addEventListener('click', () => {
-        progressContainer.classList.add('hidden');
-        keyContainer.classList.add('hidden');
-        startBtn.classList.remove('hidden');
-        keyCountSelect.classList.remove('hidden');
-        gameSelect.classList.remove('hidden');
-        generatedKeysTitle.classList.add('hidden');
-        copyAllBtn.classList.add('hidden');
-        keysList.innerHTML = '';
-        keyCountLabel.innerText = ':تعداد کلید ها';
-    });
-
-    document.getElementById('creatorChannelBtn').addEventListener('click', () => {
-        window.open('https://t.me/Airdrops_GP', '_blank');
-    });
-
-    telegramChannelBtn.addEventListener('click', () => {
-        window.open('https://t.me/AkhbarHamster', '_blank');
-    });
-
-    const generateClientId = () => {
-        const timestamp = Date.now();
-        const randomNumbers = Array.from({ length: 19 }, () => Math.floor(Math.random() * 10)).join('');
-        return `${timestamp}-${randomNumbers}`;
-    };
-
-    const login = async (clientId, appToken) => {
-        const response = await fetch('https://api.gamepromo.io/promo/login-client', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                appToken,
-                clientId,
-                clientOrigin: 'deviceid'
-            })
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to login');
-        }
-
-        const data = await response.json();
-        return data.clientToken;
-    };
-
-    const emulateProgress = async (clientToken, promoId) => {
-        const response = await fetch('https://api.gamepromo.io/promo/register-event', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${clientToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                promoId,
-                eventId: generateUUID(),
-                eventOrigin: 'undefined'
-            })
-        });
-
-        if (!response.ok) {
-            return false;
-        }
-
-        const data = await response.json();
-        return data.hasCode;
-    };
-
-    const generateKey = async (clientToken, promoId) => {
-        const response = await fetch('https://api.gamepromo.io/promo/create-code', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${clientToken}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                promoId
-            })
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to generate key');
-        }
-
-        const data = await response.json();
-        return data.promoCode;
-    };
-
-    const generateUUID = () => {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    };
-
-    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-    const delayRandom = () => Math.random() / 3 + 1;
-});
+  document.getElementById("click").innerHTML =
+    "LB/click: " + addcomma(moneyup) + " | LB/sec: " + addcomma(msec);
+  document.getElementById("total").innerHTML = "LB: " + addcomma(money);
+}
